deb8840474079e8512c6a3ab9bbc8ac401a4a56a
KyraB <bosta024@d.umn.edu>
2026-09-16 19:13:41 -0500

    Created switch case for Horizontal slice.

--- src/main/java/edu/umn/d/cs1632/Main.java
+++ src/main/java/edu/umn/d/cs1632/Main.java
@@ -28,6 +28,11 @@ public class Main {
                         int rowStart = Integer.parseInt(responseParts[2]);
                         int rowEnd = Integer.parseInt(responseParts[3]);
                         System.out.println(typeVertical(col, rowStart, rowEnd));
+                    case "H":
+                        int row = Integer.parseInt(responseParts[1]);
+                        int colStart = Integer.parseInt(responseParts[2]);
+                        int colEnd = Integer.parseInt(responseParts[3]);
+                        System.out.println(typeHorizontal(row, colStart, colEnd));
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Invalid number format. Please try again.");
@@ -45,8 +50,22 @@ public class Main {
     private static String typeVertical(int col, int rowStart, int rowEnd){
         String assumedType = null;
 
-        for (int r = rowStart; r <= rowEnd; r++){
-            String cellType = data.get(r).get(col).queryType();
+        for (int i = rowStart; i <= rowEnd; i++){
+            String cellType = data.get(i).get(col).queryType();
+            if (assumedType == null){
+                assumedType = cellType;
+            } else if (!assumedType.equals(cellType)) {
+                return "Multi";
+            }
+        }
+        return assumedType != null ? assumedType : "Empty"; //replaces if else block
+    }
+
+    private static String typeHorizontal(int row, int colStart, int colEnd){
+        String assumedType = null;
+
+        for (int i = colStart; i <= colEnd; i++){
+            String cellType = data.get(i).get(row).queryType();
             if (assumedType == null){
                 assumedType = cellType;
             } else if (!assumedType.equals(cellType)) {
